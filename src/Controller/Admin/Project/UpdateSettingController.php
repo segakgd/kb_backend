4651538462a94a592cb2c7281b0e2e9183c73e89
sega <hpsegeyy@yandex.ru>
2023-12-06 17:15:34 +0200

    Update UpdateSettingController.php

--- src/Controller/Admin/Project/UpdateSettingController.php
+++ src/Controller/Admin/Project/UpdateSettingController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller\Admin\Project;
 
-use App\Dto\Admin\Project\Request\ProjectSettingUpdateDto;
-use App\Dto\Admin\Project\Response\ProjectSettingDto;
+use App\Dto\Admin\Project\Request\ProjectSettingUpdateReqDto;
+use App\Dto\Admin\Project\Response\ProjectSettingRespDto;
 use App\Entity\User\Project;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Attributes as OA;
@@ -16,14 +16,14 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[OA\Tag(name: 'Project')]
 #[OA\RequestBody(
     content: new Model(
-        type: ProjectSettingUpdateDto::class,
+        type: ProjectSettingUpdateReqDto::class,
     )
 )]
 #[OA\Response(
     response: Response::HTTP_OK,
     description: 'Возвращает обновлённые нестройки',
     content: new Model(
-        type: ProjectSettingDto::class,
+        type: ProjectSettingRespDto::class,
     )
 )]
 class UpdateSettingController extends AbstractController
@@ -33,7 +33,7 @@ class UpdateSettingController extends AbstractController
     public function execute(Project $project): JsonResponse
     {
         return new JsonResponse(
-            new ProjectSettingDto()
+            new ProjectSettingRespDto()
         );
     }
 }
