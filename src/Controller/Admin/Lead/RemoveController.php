b31a8a9099df04a9bb9bb94d7d670d4a5612e89f
sega <hpsegeyy@yandex.ru>
2023-12-07 16:27:31 +0200

    Удаление

--- src/Controller/Admin/Lead/RemoveController.php
+++ src/Controller/Admin/Lead/RemoveController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller\Admin\Lead;
 
-use App\Dto\one\LeadFullRespDto;
 use App\Entity\User\Project;
-use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -14,15 +12,12 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[OA\Tag(name: 'Lead')]
 #[OA\Response(
-    response: Response::HTTP_OK,
-    description: 'Возвращает коллекцию заявок',
-    content: new Model(
-        type: LeadFullRespDto::class
-    ),
+    response: Response::HTTP_NO_CONTENT,
+    description: 'Если получилось удалить возвращаем 204',
 )]
-class OneListController extends AbstractController
+class RemoveController extends AbstractController
 {
-    #[Route('/api/admin/project/{project}/lead/{leadId}/', name: 'admin_lead_get_one', methods: ['GET'])]
+    #[Route('/api/admin/project/{project}/lead/{leadId}/', name: 'admin_lead_remove', methods: ['DELETE'])]
     #[IsGranted('existUser', 'project')]
     public function execute(Project $project, int $leadId): JsonResponse
     {
