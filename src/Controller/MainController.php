fcc636c1c516320991d5f65c748ee1b16ccf3edc
sega <hpsegeyy@yandex.ru>
2023-12-27 21:34:16 +0200

    +

---
+++ src/Controller/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @throws Exception
+     */
+    #[Route('/', name: 'app_main', methods: ['GET'])]
+    public function addWebhookAction(Request $request): Response
+    {
+        return new Response(
+            '
+                <body>
+                    <h1 style="position: absolute; top: 0px; left: 0px; right: 0px; bottom: 0px; display: flex; flex-wrap: wrap; align-items: center; justify-content: center; color: #c1c1c1; font-size: 11vw;">Тут нечего делать</h1>
+                </body>
+            '
+        );
+    }
+}
