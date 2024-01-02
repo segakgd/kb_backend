388f6fce66431c66f09dbfd04b0d84f939a9b434
sega <hpsegeyy@yandex.ru>
2024-01-02 16:09:57 +0200

    тест

---
+++ tests/Functional/Admin/Bot/CreateControllerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Tests\Functional\Admin\Bot;
+
+use App\Tests\Functional\ApiTestCase;
+use App\Tests\Functional\Trait\Project\ProjectTrait;
+use App\Tests\Functional\Trait\User\UserTrait;
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+
+class CreateControllerTest extends ApiTestCase
+{
+    use UserTrait;
+    use ProjectTrait;
+
+    /**
+     * @dataProvider positive
+     *
+     * @throws Exception
+     */
+    public function test(array $requestContent)
+    {
+        $client = static::createClient();
+        $entityManager = $this->getEntityManager();
+
+        $user = $this->createUser($entityManager);
+        $project = $this->createProject($entityManager, $user);
+
+        $entityManager->flush();
+
+        $client->loginUser($user);
+
+        $client->request(
+            'POST',
+            '/api/admin/project/'. $project->getId() .'/bot/',
+            [],
+            [],
+            [],
+            json_encode($requestContent)
+        );
+
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $client->getResponse()->getStatusCode());
+
+        // todo когда будет готова реализация - проверить изменения в базе
+    }
+
+    private function positive(): iterable
+    {
+        yield [
+            [
+                'type' => "telegram",
+                'token' => "0000000000:0000000000-0000000000",
+            ],
+        ];
+    }
+}
