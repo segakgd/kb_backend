1a9c61301aba18c76bbb27a4dd2ab2450a98ff41
sega <hpsegeyy@yandex.ru>
2024-01-02 16:13:26 +0200

    Удаление

--- src/Controller/Admin/Bot/RemoveController.php
+++ src/Controller/Admin/Bot/RemoveController.php
@@ -2,9 +2,27 @@
 
 namespace App\Controller\Admin\Bot;
 
+use App\Entity\User\Project;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[OA\Tag(name: 'Bot')]
+#[OA\Response(
+    response: Response::HTTP_NO_CONTENT,
+    description: 'Если получилось удалить возвращаем 204',
+)]
 class RemoveController extends AbstractController
 {
-    // todo описать
+    #[Route('/api/admin/project/{project}/bot/{botId}/', name: 'admin_bot_remove', methods: ['DELETE'])]
+    #[IsGranted('existUser', 'project')]
+    public function execute(Project $project, int $botId): JsonResponse
+    {
+        // todo ... тут мы должны обратиться к сервису или менеджеру ...
+
+        return new JsonResponse([], Response::HTTP_NO_CONTENT);
+    }
 }
---
+++ tests/Functional/Admin/Bot/RemoveControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Functional\Admin\Bot;
+
+use App\Tests\Functional\ApiTestCase;
+use App\Tests\Functional\Trait\Project\ProjectTrait;
+use App\Tests\Functional\Trait\User\UserTrait;
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+
+class RemoveControllerTest extends ApiTestCase
+{
+    use UserTrait;
+    use ProjectTrait;
+
+    /**
+     * @throws Exception
+     */
+    public function testOneDelete()
+    {
+        $client = static::createClient();
+        $entityManager = $this->getEntityManager();
+
+        $user = $this->createUser($entityManager);
+        $project = $this->createProject($entityManager, $user);
+
+        $entityManager->flush();
+
+        $client->loginUser($user);
+
+        $client->request(
+            'DELETE',
+            '/api/admin/project/'. $project->getId() .'/bot/' . 1 . '/',
+        );
+
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $client->getResponse()->getStatusCode());
+    }
+}
