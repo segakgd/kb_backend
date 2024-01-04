2ee534aac048158aa16fe88131c311681df002e5
sega <hpsegeyy@yandex.ru>
2024-01-04 22:11:06 +0200

    +

--- src/EventSubscriber/InitBotSubscriber.php
+++ src/EventSubscriber/InitBotSubscriber.php
@@ -2,11 +2,18 @@
 
 namespace App\EventSubscriber;
 
+use App\Dto\Core\Telegram\Webhook\WebhookDto;
 use App\Event\InitBotEvent;
+use App\Service\Integration\Telegram\TelegramServiceInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class InitBotSubscriber implements EventSubscriberInterface
 {
+    public function __construct(
+        private readonly TelegramServiceInterface $telegramService
+    ){
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -16,5 +23,14 @@ class InitBotSubscriber implements EventSubscriberInterface
 
     public function initBot(InitBotEvent $event): void
     {
+        $dot = $event->getBot();
+
+        $uri = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/webhook/' . $dot->getProjectId() . '/' . $dot->getType() . '/';
+
+        $webhookDto = (new WebhookDto())
+            ->setUrl($uri)
+        ;
+
+        $this->telegramService->setWebhook($webhookDto, $dot->getToken());
     }
 }
