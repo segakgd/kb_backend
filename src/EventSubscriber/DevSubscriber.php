c7facba90812a2de5faaa36f988cf03a62278025
sega <hpsegeyy@yandex.ru>
2024-01-13 22:19:38 +0200

    KB-37 блокирую всё кроме вебхука и своего ip

---
+++ src/EventSubscriber/DevSubscriber.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Exception;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Не старался, нужно было быстро и просто
+ */
+class DevSubscriber implements EventSubscriberInterface
+{
+    private const ALLOWED_IP = [
+        '178.68.18.84',
+    ];
+
+    private const OPEN_UR = [
+        '/webhook/1/telegram/',
+    ];
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => 'checkRequest',
+        ];
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function checkRequest(RequestEvent $event): void
+    {
+        if (!$this->checkUri($event) && !$this->checkIp($event)){
+            $event->setResponse(new Response(null, Response::HTTP_NOT_FOUND));
+        }
+    }
+
+    private function checkIp(RequestEvent $event): bool
+    {
+        return in_array($event->getRequest()->getClientIp(), self::ALLOWED_IP);
+    }
+
+    private function checkUri(RequestEvent $event): bool
+    {
+        return in_array($event->getRequest()->getRequestUri(), self::OPEN_UR);
+    }
+}
