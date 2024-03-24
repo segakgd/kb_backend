2f8c5e2b388b68fc4160422568536328f1fe01d8
sega <hpsegeyy@yandex.ru>
2024-03-24 23:00:51 +0200

    Update LeadService.php

--- src/Service/Admin/Ecommerce/Lead/LeadService.php
+++ src/Service/Admin/Ecommerce/Lead/LeadService.php
@@ -2,7 +2,6 @@
 
 namespace App\Service\Admin\Ecommerce\Lead;
 
-use App\Entity\Lead\Deal;
 use App\Repository\Lead\DealEntityRepository;
 
 class LeadService implements LeadServiceInterface
@@ -10,16 +9,5 @@ class LeadService implements LeadServiceInterface
     public function __construct(
         public readonly DealEntityRepository $dealEntityRepository,
     ) {
-    }
-
-    public function create()
-    {
-        $deal = ( new Deal())
-            ->setProjectId()
-            ->setContacts()
-            ->setOrder()
-        ;
-
-        dd('asdas');
     }
 }
