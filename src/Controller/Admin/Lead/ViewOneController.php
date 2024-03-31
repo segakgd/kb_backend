68dd693bbaa888df7d04d79b9856a4f0ebda96e5
oleg <onderoriginal@gmail.com>
2024-03-31 18:01:54 +0200

    create lead mapper for response

--- src/Controller/Admin/Lead/ViewOneController.php
+++ src/Controller/Admin/Lead/ViewOneController.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin\Lead;
 
-use App\Controller\Admin\Lead\DTO\Response\Fields\LeadContactsRespDto;
-use App\Controller\Admin\Lead\DTO\Response\Fields\LeadFieldRespDto;
 use App\Controller\Admin\Lead\DTO\Response\LeadRespDto;
-use App\Controller\Admin\Lead\DTO\Response\Order\OrderRespDto;
 use App\Entity\Lead\Deal;
 use App\Entity\User\Project;
-use App\Repository\Lead\ContactsEntityRepository;
+use App\Service\Admin\Lead\LeadMapper;
 use Doctrine\ORM\EntityManagerInterface;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Attributes as OA;
@@ -32,8 +29,8 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class ViewOneController extends AbstractController
 {
     public function __construct(
-        private readonly ContactsEntityRepository $contactsEntityRepository,
-        private readonly EntityManagerInterface  $manager,
+        private readonly EntityManagerInterface $manager,
+        private readonly LeadMapper             $leadMapper,
     )
     {
     }
@@ -50,85 +47,6 @@ class ViewOneController extends AbstractController
             throw new AccessDeniedException('Access Denied.');
         }
 
-        return $this->json($this->mapToResponse($lead));
-    }
-
-    private function mapToResponse(Deal $deal): LeadRespDto // todo точно в сервис перекинуть эту кашу!
-    {
-        $leadContactsRespDto = $this->mapContactsToResponse($deal);
-        $fieldsRespArray = $this->mapFieldsToResponse($deal);
-        $orderDto = $this->mapOrderToResponse($deal);
-
-        return (new LeadRespDto())
-            ->setContacts($leadContactsRespDto)
-            ->setFields($fieldsRespArray)
-            ->setNumber($deal->getId())
-            ->setOrder($orderDto);
-    }
-
-    private function mapContactsToResponse(Deal $deal): LeadContactsRespDto
-    {
-        $leadContactsRespDto = new LeadContactsRespDto();
-        $contacts = $deal->getContacts();
-
-        if (null === $contacts) {
-            return $leadContactsRespDto;
-        }
-
-        $this->manager->refresh($contacts); // temporary fix
-
-        if ($contacts->getEmail()) {
-            $emailField = (new LeadFieldRespDto())
-                ->setName('email')
-                ->setType('email')
-                ->setValue($contacts->getEmail());
-
-            $leadContactsRespDto->setMail($emailField);
-        }
-
-        if ($contacts->getPhone()) {
-            $phoneField = (new LeadFieldRespDto())
-                ->setName('phone')
-                ->setType('phone')
-                ->setValue($contacts->getPhone());
-
-            $leadContactsRespDto->setPhone($phoneField);
-        }
-
-        if ($contacts->getLastName() || $contacts->getFirstName()) {
-            $fullName = ($contacts->getFirstName() ?? '') . ' ' . ($contacts->getLastName() ?? '');
-
-            $fullNameField = (new LeadFieldRespDto)
-                ->setName('fullName')
-                ->setType('full_name')
-                ->setValue($fullName);
-
-            $leadContactsRespDto->setFullName($fullNameField);
-        }
-
-        return $leadContactsRespDto;
-    }
-
-    private function mapFieldsToResponse(Deal $deal): array
-    {
-        $fields = $deal->getFields();
-
-        $fieldsArray = [];
-
-        foreach ($fields as $field) {
-            $fieldDto = (new LeadFieldRespDto())
-                ->setType('string')
-                ->setValue($field->getValue())
-                ->setName($field->getName());
-
-            $fieldsArray[] = $fieldDto;
-        }
-
-        return $fieldsArray;
-    }
-
-    private function mapOrderToResponse(Deal $deal): OrderRespDto // надо подумать тут, а то еще непонятно, какое дто лежит в ордере в базе
-    {
-        return new OrderRespDto();
+        return $this->json($this->leadMapper->mapToResponse($lead));
     }
 }
---
+++ src/Service/Admin/Lead/LeadMapper.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Admin\Lead;
+
+use App\Controller\Admin\Lead\DTO\Response\Fields\LeadContactsRespDto;
+use App\Controller\Admin\Lead\DTO\Response\Fields\LeadFieldRespDto;
+use App\Controller\Admin\Lead\DTO\Response\LeadRespDto;
+use App\Controller\Admin\Lead\DTO\Response\Order\OrderRespDto;
+use App\Entity\Lead\Deal;
+use Doctrine\ORM\EntityManagerInterface;
+
+class LeadMapper
+{
+    public function __construct(private readonly EntityManagerInterface $manager,)
+    {
+
+    }
+
+    public function mapToResponse(Deal $deal): LeadRespDto
+    {
+        $leadContactsRespDto = $this->mapContactsToResponse($deal);
+        $fieldsRespArray = $this->mapFieldsToResponse($deal);
+        $orderDto = $this->mapOrderToResponse($deal);
+
+        return (new LeadRespDto())
+            ->setContacts($leadContactsRespDto)
+            ->setFields($fieldsRespArray)
+            ->setNumber($deal->getId())
+            ->setOrder($orderDto);
+    }
+
+    private function mapContactsToResponse(Deal $deal): LeadContactsRespDto
+    {
+        $leadContactsRespDto = new LeadContactsRespDto();
+        $contacts = $deal->getContacts();
+
+        if (null === $contacts) {
+            return $leadContactsRespDto;
+        }
+
+        $this->manager->refresh($contacts); // temporary fix
+
+        if ($contacts->getEmail()) {
+            $emailField = (new LeadFieldRespDto())
+                ->setName('email')
+                ->setType('email')
+                ->setValue($contacts->getEmail());
+
+            $leadContactsRespDto->setMail($emailField);
+        }
+
+        if ($contacts->getPhone()) {
+            $phoneField = (new LeadFieldRespDto())
+                ->setName('phone')
+                ->setType('phone')
+                ->setValue($contacts->getPhone());
+
+            $leadContactsRespDto->setPhone($phoneField);
+        }
+
+        if ($contacts->getLastName() || $contacts->getFirstName()) {
+            $fullName = ($contacts->getFirstName() ?? '') . ' ' . ($contacts->getLastName() ?? '');
+
+            $fullNameField = (new LeadFieldRespDto)
+                ->setName('fullName')
+                ->setType('full_name')
+                ->setValue($fullName);
+
+            $leadContactsRespDto->setFullName($fullNameField);
+        }
+
+        return $leadContactsRespDto;
+    }
+
+    private function mapFieldsToResponse(Deal $deal): array
+    {
+        $fields = $deal->getFields();
+
+        $fieldsArray = [];
+
+        foreach ($fields as $field) {
+            $fieldDto = (new LeadFieldRespDto())
+                ->setType('string')
+                ->setValue($field->getValue())
+                ->setName($field->getName());
+
+            $fieldsArray[] = $fieldDto;
+        }
+
+        return $fieldsArray;
+    }
+
+    private function mapOrderToResponse(Deal $deal): OrderRespDto // надо подумать тут, а то еще непонятно, какое дто лежит в ордере в базе
+    {
+        return new OrderRespDto();
+    }
+}
