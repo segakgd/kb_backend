30b87780a5c038d6b77ccdde1c6b3a4ab707192c
sega <hpsegeyy@yandex.ru>
2024-04-02 21:36:41 +0200

    Update ScenarioResolver.php

--- src/Service/System/Resolver/ScenarioResolver.php
+++ src/Service/System/Resolver/ScenarioResolver.php
@@ -2,37 +2,51 @@
 
 namespace App\Service\System\Resolver;
 
+use App\Dto\Contract\ContractMessageDto;
 use App\Helper\KeyboardHelper;
 use App\Helper\MessageHelper;
 use App\Service\System\Resolver\Dto\Contract;
 
-/**
- * @deprecated need refactoring
- */
 class ScenarioResolver
 {
     public function resolve(Contract $contract, array $scenarioStep): Contract
     {
         $contractMessage = MessageHelper::createContractMessage();
 
-        if ($scenarioStep['message']) {
-            $contractMessage->setMessage($scenarioStep['message']);
-        }
-
-        if (!empty($scenarioStep['keyboard'])) {
-            $replyMarkups = KeyboardHelper::mapKeyboard($scenarioStep);
-
-            if (!empty($replyMarkups)) {
-                $contractMessage->setKeyBoard($replyMarkups);
-            }
-        }
-
-        if (!empty($scenarioStep['attached'])) {
-            dd('сработали attached', $scenarioStep['attached']);
-        }
+        $this->setMessage($contractMessage, $scenarioStep);
+        $this->setKeyboard($contractMessage, $scenarioStep);
 
         $contract->getResult()->addMessage($contractMessage);
 
         return $contract;
+    }
+
+    private function setMessage(ContractMessageDto $contractMessage, array $scenarioStep): void
+    {
+        if (!$scenarioStep['message']) {
+            return;
+        }
+
+        $contractMessage->setMessage($scenarioStep['message']);
+    }
+
+    private function setKeyboard(ContractMessageDto $contractMessage, array $scenarioStep): void
+    {
+        if (empty($scenarioStep['keyboard'])) {
+            return;
+        }
+
+        $replyMarkups = KeyboardHelper::mapKeyboard($scenarioStep);
+
+        if (empty($replyMarkups)) {
+            return;
+        }
+
+        $contractMessage->setKeyBoard($replyMarkups);
+    }
+
+    private function handleAttached(array $attachedData): void
+    {
+        dd('сработали attached', $attachedData);
     }
 }
