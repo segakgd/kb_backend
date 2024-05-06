5cada1a6328f5da4f50b9fa4a6b53ddc71e1b96b
oleg <onderoriginal@gmail.com>
2024-05-06 19:58:03 +0200

    added login controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
@@ -26,5 +29,43 @@ class SecurityController extends AbstractController
         $request->getSession()->invalidate();
 
         return $this->redirectToRoute('login_admin_form');
+    }
+
+    #[Route('/api/user/authenticate/', name: 'app_login.authenticate', methods: ['POST'])]
+    public function loginAuthenticate(Request $request, JWTTokenManagerInterface $JWTManager, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        // Check if email and password are present in the request
+        if (!isset($data['email']) || !isset($data['password'])) {
+            throw new BadCredentialsException('Email and password are required.');
+        }
+
+        // Retrieve user by email
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $data['email']]);
+//        dd($user);
+        // Check if user exists and password is correct
+        if (!$user || !$passwordHasher->isPasswordValid($user, $data['password'])) {
+            throw new BadCredentialsException('Invalid email or password.');
+        }
+
+        // Generate JWT tokens
+        $accessToken = $JWTManager->create($user); // Generate access token
+        $refreshToken = $JWTManager->create($user); // Generate refresh token
+
+        $refreshEntity = (new RefreshToken())
+            ->setRefreshToken($refreshToken)
+            ->setValid(new DateTime('+1 day'))
+            ->setUsername($user->getId())
+        ;
+
+        $entityManager->persist($refreshEntity);
+        $entityManager->flush();
+
+        // Return tokens in response
+        return new JsonResponse([
+            'access_token' => $accessToken,
+            'refresh_token' => $refreshToken,
+        ]);
     }
 }
