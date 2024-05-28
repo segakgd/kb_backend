df29584a7d6ded7ed444f5fa15b0f1c1c2f21040
Sega <hpsergeyy@yandex.ru>
2024-05-29 01:06:08 +0200

    separate auth controller

--- src/Controller/Security/SecurityApiController.php
+++ src/Controller/Security/SecurityApiController.php
@@ -4,18 +4,18 @@ namespace App\Controller\Security;
 
 use App\Controller\GeneralController;
 use App\Controller\Security\DTO\AuthDto;
+use App\Dto\Security\UserDto;
+use App\Exception\Security\UserExistException;
 use App\Service\Common\Security\SecurityService;
 use Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class SecurityController extends GeneralController
+class SecurityApiController extends GeneralController
 {
     public function __construct(
         private readonly ValidatorInterface $validator,
@@ -26,24 +26,6 @@ class SecurityController extends GeneralController
             $this->serializer,
             $this->validator,
         );
-    }
-
-    #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('admin/user/auth.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]);
-    }
-
-    #[Route('/logout', name: 'app_logout')]
-    public function logout(): RedirectResponse
-    {
-        return new RedirectResponse("/");
     }
 
     /**
@@ -62,5 +44,22 @@ class SecurityController extends GeneralController
                 'token' => $this->securityService->refresh($user),
             ]
         );
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('/api/user/registration/', name: 'visitor_registration', methods: "POST")]
+    public function exist(Request $request): JsonResponse
+    {
+        $userDto = $this->getValidDtoFromRequest($request, UserDto::class);
+
+        try {
+            $user = $this->securityService->createUser($userDto);
+        } catch (UserExistException $exception) {
+            return $this->json(['message' => $exception->getMessage()], Response::HTTP_BAD_REQUEST);
+        }
+
+        return $this->json($user, 200, [], ['groups' => ['openForReading']]);
     }
 }
