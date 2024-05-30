8b35d0cbd3b60788b7dbcd20d2722b40091b64c1
Sega <hpsergeyy@yandex.ru>
2024-05-31 01:29:52 +0200

    twig

--- src/Controller/Dev/DashboardController.php
+++ src/Controller/Dev/DashboardController.php
@@ -4,10 +4,13 @@ namespace App\Controller\Dev;
 
 use App\Entity\User\Bot;
 use App\Entity\User\Project;
+use App\Entity\User\User;
 use App\Entity\Visitor\VisitorSession;
 use App\Repository\User\ProjectRepository;
 use App\Repository\User\UserRepository;
 use App\Service\Admin\DashboardService;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,24 +19,42 @@ class DashboardController extends AbstractController
 {
     public function __construct(
         private readonly DashboardService $dashboardService,
+        private readonly ProjectRepository $projectRepository,
     ) {
     }
 
+    /**
+     * @throws NonUniqueResultException
+     * @throws NoResultException
+     */
     #[Route('/admin/users/', name: 'admin_users')]
     public function users(UserRepository $userRepository): Response
     {
         $users = $userRepository->findAll();
+        $usersResponse = [];
+
+        foreach ($users as $user) {
+            $role = in_array('ROLE_ADMIN', $user->getRoles()) ? 'admin' : 'user';
+
+            $usersResponse[] = [
+                'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'role' => $role,
+                'created_at' => $user->getCreatedAt(),
+                'projects_count' => $this->projectRepository->projectsCountByUser($user),
+            ];
+        }
 
         return $this->render(
             'admin/user/users.html.twig',
             [
-                'users' => $users,
+                'users' => $usersResponse,
             ]
         );
     }
 
-    #[Route('/admin/projects/', name: 'admin_projects')]
-    public function projects(ProjectRepository $projectRepository): Response
+    #[Route('/admin/user/{user}/projects/', name: 'admin_projects')]
+    public function projects(User $user, ProjectRepository $projectRepository): Response
     {
         $projectRepository->findAll();
 
