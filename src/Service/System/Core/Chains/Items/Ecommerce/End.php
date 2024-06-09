d95213a889246d3e9d92c088deacb632a719372a
Sega <hpsergeyy@yandex.ru>
2024-06-09 02:00:53 +0200

    copy old chains

---
+++ src/Service/System/Core/Chains/Items/Ecommerce/End.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service\System\Core\Chains\Items\Items;
+
+use App\Dto\SessionCache\Cache\CacheDto;
+use App\Helper\MessageHelper;
+use App\Service\System\Core\Dto\Responsible;
+use Exception;
+
+class End // extends AbstractChain // todo удалить
+{
+    /**
+     * @throws Exception
+     */
+    public function success(Responsible $responsible, CacheDto $cacheDto): bool
+    {
+        return true;
+    }
+
+    public function fall(Responsible $responsible, CacheDto $cacheDto): bool
+    {
+        $replyMarkups = [
+            [
+                [
+                    'text' => 'вернуться в главное меню'
+                ],
+            ]
+        ];
+
+        $responsibleMessage = MessageHelper::createResponsibleMessage(
+            'Не понимаю о чем вы... мб вам...',
+            null,
+            $replyMarkups,
+        );
+
+        $responsible->getResult()->addMessage($responsibleMessage);
+
+        return false;
+    }
+
+    public function validateCondition(string $content): bool
+    {
+        return false;
+    }
+}
