1f5990d029a3a3477ea51a61a690dbdffc867cfb
Sega <hpsergeyy@yandex.ru>
2024-08-17 02:10:31 +0200

    Update AbstractResponse.php

--- src/Controller/AbstractResponse.php
+++ src/Controller/AbstractResponse.php
@@ -9,6 +9,11 @@ abstract class AbstractResponse
         return new static();
     }
 
+    public static function mapFromArray(array $data): static
+    {
+        return new static();
+    }
+
     public static function mapCollection(iterable $collection): array
     {
         $mapResult = [];
@@ -19,6 +24,21 @@ abstract class AbstractResponse
             }
 
             $mapResult = self::mapFromEntity($item);
+        }
+
+        return $mapResult;
+    }
+
+    public static function mapCollectionArray(iterable $collection): array
+    {
+        $mapResult = [];
+
+        foreach ($collection as $item) {
+            if (!is_array($item)) {
+                continue;
+            }
+
+            $mapResult = self::mapFromArray($item);
         }
 
         return $mapResult;
